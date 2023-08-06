50fdb29cbf50f88794b1193d6641db3f056389d8
seunghoon.choi <truknight0502@gmail.com>
2023-08-07 00:28:43 +0900

    init commit

--- src/components/login/Login.svelte
+++ src/components/login/Login.svelte
@@ -50,13 +50,6 @@
         margin: 0 auto;
     }
 
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
-
     .input-div {
         margin-top: 40px;
     }
